cf8c6d2ebbd5ea46e897a785f8b040d28a8c3a43
Victor Martinez <martinezvazquez122@gmail.com>
2018-01-03 12:19:51 -0500

    setting up firebase

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -22,7 +22,6 @@ export class GithubFollowersComponent implements OnInit {
     Observable.combineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-      
     ])
     .switchMap(combined => {
       const id = combined[0].get('id');
